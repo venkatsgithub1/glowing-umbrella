1485863ea795e1bae61378431131279bc4572695
ven <venkatsgithub@gmail.com>
2026-01-03 10:42:49 +0530

    Solution for reorder list problem

--- src/main/java/io/learn/linkedlist/ReorderList.java
+++ src/main/java/io/learn/linkedlist/ReorderList.java
@@ -1,12 +1,18 @@
 package io.learn.linkedlist;
 
+import java.util.List;
+
 public class ReorderList {
     public static void main(String[] args) {
-
+        reorderList(ListNode.buildLinkedList(List.of(1,2,3,4,5,6)));
+        reorderList(ListNode.buildLinkedList(List.of(1,2,3,4,5)));
+        reorderList(ListNode.buildLinkedList(List.of(1,2,3,4)));
+        reorderList(ListNode.buildLinkedList(List.of(1)));
+        reorderList(ListNode.buildLinkedList(List.of(1, 2)));
     }
 
     private static void reorderList(ListNode node) {
-        if (node == null) {
+        if (node == null || node.next == null) {
             return;
         }
 
@@ -19,7 +25,41 @@ public class ReorderList {
             fast = fast.next.next;
         }
 
-        reverse(slow);
+        prev.next = null;
+
+        slow = reverse(slow);
+        prev = null;
+
+        ListNode current = node;
+        while (current != null && slow != null) {
+            ListNode nextNodeOfCurrent = current.next;
+            ListNode nextNodeOfSlow = slow.next;
+
+            slow.next = null;
+
+            current.next = slow;
+            current.next.next = nextNodeOfCurrent;
+
+            prev = current.next;
+            current = nextNodeOfCurrent;
+            slow = nextNodeOfSlow;
+        }
+
+        current = prev;
+
+        while (slow != null) {
+            ListNode nextNodeOfCurrent = current.next;
+            ListNode nextNodeOfSlow = slow.next;
+
+            slow.next = null;
+
+            current.next = slow;
+            current.next.next = nextNodeOfCurrent;
+
+            current = nextNodeOfCurrent;
+            slow = nextNodeOfSlow;
+        }
+        System.out.println(node);
     }
 
     private static ListNode reverse(ListNode node) {
