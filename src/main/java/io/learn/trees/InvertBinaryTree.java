9911f52368180ff18ca150a90e3f50763af8889d
ven <venkatsgithub@gmail.com>
2026-02-07 22:40:17 +0530

    Adding tree representation

---
+++ src/main/java/io/learn/trees/InvertBinaryTree.java
@@ -0,0 +1,10 @@
+package io.learn.trees;
+
+import java.util.List;
+
+public class InvertBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.constructTree(List.of(4,2,7,1,3,6,9));
+        System.out.println(root);
+    }
+}
